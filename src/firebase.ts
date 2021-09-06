c655185d9a5c0fa2be4799040b090c96d2e99406
lhk3337 <lhk3337@naver.com>
2021-09-07 08:55:15 +0900

    add firebase apikey and .env

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+
+interface IFireBaseConfig {
+  apiKey: string | undefined;
+  authDomain: string | undefined;
+  projectId: string | undefined;
+  storageBucket: string | undefined;
+  messagingSenderId: string | undefined;
+  appId: string | undefined;
+}
+const firebaseConfig: IFireBaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+export default firebase.initializeApp(firebaseConfig);
