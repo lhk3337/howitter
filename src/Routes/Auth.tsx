03fc1e5eb9e755d791153aac9205194812334371
lhk3337 <lhk3337@naver.com>
2021-09-08 19:07:10 +0900

    create auth email and password

--- src/Routes/Auth.tsx
+++ src/Routes/Auth.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import { authService } from "firebaseAPI";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [newAccount, setNewAccount] = useState<boolean>(true);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      if (newAccount) {
+        await authService.createUserWithEmailAndPassword(email, password);
+      } else {
+        await authService.signInWithEmailAndPassword(email, password);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input name="email" type="text" placeholder="Email" required value={email} onChange={onChange} />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+          autoComplete="on"
+        />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
