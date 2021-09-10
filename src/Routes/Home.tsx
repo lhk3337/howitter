272cf829fd828e73478e625b74c06ae307718a7a
lhk3337 <lhk3337@naver.com>
2021-09-10 17:26:56 +0900

    home form 추가와 firebase db import

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [howitter, setHowitter] = useState<string>("");
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = event;
+    setHowitter(value);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" value={howitter} onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
+      <input type="submit" value="ho_witter" />
+    </form>
+  );
 };
 
 export default Home;
--- src/firebaseAPI.ts
+++ src/firebaseAPI.ts
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/database";
 interface IFireBaseConfig {
   apiKey: string | undefined;
   authDomain: string | undefined;
