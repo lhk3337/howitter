4bafa198e2f71d34c2f3aba3be81feaf4e63a31c
lhk3337 <lhk3337@naver.com>
2021-09-10 11:25:02 +0900

    profile component route 설정

--- src/Components/Router.tsx
+++ src/Components/Router.tsx
@@ -2,19 +2,23 @@ import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "Routes/Auth";
 import Home from "Routes/Home";
+import Navigation from "Components/Navigation";
+import Profile from "Routes/Profile";
 interface Iprops {
   isLoggedIn: boolean;
 }
 const AppRouter = ({ isLoggedIn }: Iprops) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/" component={Home} />
+            <Route exact path="/profile" component={Profile} />
           </>
         ) : (
-          <Route exact path="/" component={Auth} />
+          <Route exact path="/" component={Auth} /> //로그인 화면
         )}
       </Switch>
     </Router>
