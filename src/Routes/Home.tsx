6b8b3925c9caf135448382a0f57db8a7621af41b
lhk3337 <lhk3337@naver.com>
2021-09-13 23:44:41 +0900

    firebase create, read 추가

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,11 +1,31 @@
 import { dbService } from "firebaseAPI";
-import React, { useState } from "react";
-import { TFormEvent, TChangeEvent } from "types/type";
+import React, { useEffect, useState } from "react";
+import { TFormEvent, TChangeEvent, IhowitterMessage } from "types/type";
 
 const Home = () => {
   const [howitter, setHowitter] = useState<string>("");
-  const onSubmit = (event: TFormEvent) => {
+  const [howitters, setHowitters] = useState<IhowitterMessage[]>([]);
+
+  const getHowitters = async () => {
+    const dbHowitter = await dbService.collection("howitter").get();
+    dbHowitter.forEach((document) => {
+      const howitterObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setHowitters((prev: any) => [howitterObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getHowitters();
+  }, []);
+  const onSubmit = async (event: TFormEvent) => {
     event.preventDefault();
+    await dbService.collection("howitter").add({
+      message: howitter,
+      createAt: Date.now(),
+    });
+    setHowitter("");
   };
 
   const onChange = (event: TChangeEvent) => {
@@ -15,12 +35,20 @@ const Home = () => {
 
     setHowitter(value);
   };
-
   return (
-    <form onSubmit={onSubmit}>
-      <input type="text" value={howitter} onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
-      <input type="submit" value="HoWitter" />
-    </form>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={howitter} onChange={onChange} placeholder="What's on your mind?" maxLength={120} />
+        <input type="submit" value="HoWitter" />
+      </form>
+      <div>
+        {howitters.map((howitter: IhowitterMessage) => (
+          <div key={howitter.id}>
+            <h4>{howitter.message}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
