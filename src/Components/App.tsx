f6c655472a2ec87bdeb4573edf5011e40a596293
lhk3337 <lhk3337@naver.com>
2021-09-30 18:18:44 +0900

    logout 문제 수정

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -14,6 +14,8 @@ function App() {
           uid: user.uid,
           updateProfile: (args: any) => user.updateProfile(args),
         });
+      } else {
+        setUserObj(null);
       }
       setInit(true);
     });
