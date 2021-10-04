ff318270f4e11e3a0f24cdd20e8d30303c5af7f7
lhk3337 <lhk3337@naver.com>
2021-10-04 23:32:37 +0900

    공통 style 추가

--- src/styles/Common/CommonStyle.ts
+++ src/styles/Common/CommonStyle.ts
@@ -15,3 +15,19 @@ export const CommonFormInput = css`
   background-color: white;
   color: black;
 `;
+
+export const CommonFormBtn = css`
+  cursor: pointer;
+  width: 100%;
+  padding: 7px 20px;
+  text-align: center;
+  color: white;
+  border-radius: 20px;
+  background-color: #04aaff;
+  cursor: pointer;
+`;
+
+export const CommonCancelBtn = css`
+  cursor: pointer;
+  background-color: tomato;
+`;
