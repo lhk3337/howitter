e0f1398f2aaabce6d531bd2fbf514e01698d0010
lhk3337 <lhk3337@naver.com>
2021-10-05 19:37:42 +0900

    Router 컴포넌트를 styled component로 적용

--- src/Components/Router.tsx
+++ src/Components/Router.tsx
@@ -5,17 +5,17 @@ import Home from "Routes/Home";
 import Navigation from "Components/Navigation";
 import Profile from "Routes/Profile";
 import { Iprops } from "types";
-
+import * as Style from "styles/Components/RouterStyle";
 const AppRouter = ({ refreshUser, isLoggedIn, userObj }: Iprops) => {
   return (
     <Router>
       {isLoggedIn && <Navigation userObj={userObj} />}
       <Switch>
         {isLoggedIn ? (
-          <>
+          <Style.RouterContainer>
             <Route exact path="/" render={() => <Home userObj={userObj} />} />
             <Route exact path="/profile" render={() => <Profile userObj={userObj} refreshUser={refreshUser} />} />
-          </>
+          </Style.RouterContainer>
         ) : (
           <Route exact path="/" component={Auth} /> //로그인 화면
         )}
--- src/styles/Components/RouterStyle.ts
+++ src/styles/Components/RouterStyle.ts
@@ -1,1 +1,10 @@
-export {};
+import styled from "styled-components";
+
+export const RouterContainer = styled.div`
+  display: flex;
+  max-width: 890px;
+  width: 100%;
+  margin-top: 80px;
+  margin: 0 auto;
+  justify-content: center;
+`;
